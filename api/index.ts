b18848a6cbed1d00ab880d4330699cf0401fc69d
Cutiepie <43445785+Ran-Mewo@users.noreply.github.com>
2024-06-18 15:21:13 +1000

    Initial commit

---
+++ api/index.ts
@@ -0,0 +1,42 @@
+const fetchies = require('node-fetch');
+const express = require("express");
+const app = express();
+
+console.log(encodeURIComponent("https://pastebin.com/raw/adRXHXNB"))
+
+app.get("/", (req, res) => res.send("owolewd"));
+
+async function getBase64(base64: string) {
+    if (base64.startsWith("http")) {
+        return await fetchies(base64).then(res => res.text());
+    }
+    return base64;
+}
+
+app.get('/imagebase64/:base64', async function (req, res) {
+    let base64Image = await getBase64(decodeURIComponent(req.params.base64));
+    const mimeMatch = base64Image.match(/^data:(.*?);base64,/);
+    let mimeType: string;
+
+    if (!mimeMatch) {
+        mimeType = 'image/png';
+    } else {
+        mimeType = mimeMatch[1];
+        const base64Prefix = `data:${mimeType};base64,`;
+
+        if (base64Image.startsWith(base64Prefix)) {
+            base64Image = base64Image.slice(base64Prefix.length);
+        }
+    }
+
+    const imageBuffer = Buffer.from(base64Image, 'base64');
+
+    res.writeHead(200, {
+        'Content-Type': mimeType,
+        'Content-Length': imageBuffer.length
+    });
+    res.end(imageBuffer);
+});
+
+// app.listen(3000, () => console.log("Server ready on port 3001."));
+module.exports = app;
